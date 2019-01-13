d6a96cbf5671a4d759f77f9b9ac17e23eede9c55
jry <598821125@qq.com>
2019-01-13 10:24:16 +0800

    API支持版本管理

--- route/route.php
+++ route/route.php
@@ -16,26 +16,29 @@ Route::rule('/', 'core/index/index'); // 首页访问路由
 $_module_conf_list = [];
 $module_list = [0 => ['name' => 'core']];
 $_api_pat = 'api/'; // API根路径
+$_api_pat = 'api/';
 foreach ($module_list as $key => $value) {
     $conf = require_once Env::get('app_path') . $value['name'] . '/tpvue/tpvue.php';
     $_module_conf_list[$value['name']] = $conf;
-    if (isset($conf['route']['rule']) && is_array($conf['route']['rule'])) {
-        foreach ($conf['route']['rule'] as $key1 => $value1) {
-            Route::rule($_api_pat.$key1, $value['name'].'/'.$value1[0]);
+    foreach ($conf['route'] as $key0 => $value0) {
+        if (isset($value0['rule']) && is_array($value0['rule'])) {
+            foreach ($value0['rule'] as $key1 => $value1) {
+                Route::rule($_api_pat.$key0.'/'.$value['name'].'/'.$key1, $value['name'].'/'.$value1[0]);
+            }
         }
-    }
-    if (isset($conf['route']['resource']) && is_array($conf['route']['resource'])) {
-        foreach ($conf['route']['resource'] as $key2 => $value2) {
-            Route::rule($_api_pat.$key2, $value['name'].'/'.$value2[0]);
+        if (isset($value0['resource']) && is_array($value0['resource'])) {
+            foreach ($value0['resource'] as $key2 => $value2) {
+                Route::rule($_api_pat.$key0.'/'.$value['name'].'/'.$key2, $value['name'].'/'.$value2[0]);
+            }
         }
-    }
-    if (isset($conf['route']['alias']) && is_array($conf['route']['alias'])) {
-        foreach ($conf['route']['alias'] as $key3 => $value3) {
-            // 别名路由不支持根路径
-            if (strpos($value3[1], '\\') == 0) {
-                Route::alias($key3, $value3[0]);
-            } else {
-                Route::alias($key3, $value['name'].'/'.$value3[0]);
+        if (isset($value0['alias']) && is_array($value0['alias'])) {
+            foreach ($value0['alias'] as $key3 => $value3) {
+                // 别名路由不支持根路径
+                if (strpos($value3[1], '\\') == 0) {
+                    Route::alias($key3, $value3[0]);
+                } else {
+                    Route::alias($key3, $value['name'].'/'.$value3[0]);
+                }
             }
         }
     }
