b1bbaca4f7f58742ba237cee58509c8029d88470
`2..5 <598821125@qq.com>
2019-01-16 18:51:00 +0800

    登陆初步对接

--- application/common.php
+++ application/common.php
@@ -10,3 +10,20 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+
+/**
+ * 系统非常规MD5加密方法
+ * @param  string $str 要加密的字符串
+ * @return string
+ * @author jry <598821125@qq.com>
+ */
+function user_md5($str, $auth_key = '')
+{
+    if (!$auth_key) {
+        $auth_key = env('auth_key');
+    }
+    return !$str ? false : md5(sha1($str) . $auth_key);
+}
+
+
+
