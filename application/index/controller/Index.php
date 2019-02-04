4b00db8e21ef261ea3808c44065e63ae7a5a6a9c
jry <ijry@qq.com>
2019-02-04 18:47:45 +0800

    修改文件头部注释

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,11 +1,23 @@
 <?php
+/**
+ * +----------------------------------------------------------------------
+ * | InitAdmin/actionphp [ InitAdmin渐进式模块化通用后台 ]
+ * +----------------------------------------------------------------------
+ * | Copyright (c) 2018-2019 http://initadmin.net All rights reserved.
+ * +----------------------------------------------------------------------
+ * | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ * +----------------------------------------------------------------------
+ * | Author: jry <ijry@qq.com>
+ * +----------------------------------------------------------------------
+*/
+
 namespace app\index\controller;
 
 class Index
 {
     public function index()
     {
-        return 'tpvue后台Restful接口运行中...';
+        return 'InitAdmin后台接口运行中...';
     }
 
 }
