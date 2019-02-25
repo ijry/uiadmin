25a7ba9476ae6bd7d20d24b9f49c85401df28f29
jry <ijry@qq.com>
2019-02-25 19:03:30 +0800

    支持后台编写API接口文档初步

--- application/core/install/install.php
+++ application/core/install/install.php
@@ -34,36 +34,34 @@ return [
                     'description' => '使用凭证登录系统', // 接口功能描述
                     'params' => [
                         'account' => [
-                            'is_must' => 1, //该参数是否必须
+                            'require' => true, //该参数是否必须
                             'title' => '账号', //字段名称
                             'description' => '账号支持手机号/邮箱/用户名', // 字段描述
                             'example' => 'test', // 字段传值示例
                         ],
                         'password' => [
-                            'is_must' => 1, //该参数是否必须
+                            'require' => true, //该参数是否必须
                             'title' => '密码哈希', //字段名称
                             'description' => '密码必须在前台用公钥加密传输，这样系统跟安全，密码不容易在传输中被劫持。', // 字段描述
                             'example' => 'test123', // 字段传值示例
                         ]
                     ],
-                    'return' => [
-                        'success' => [
-                            'code' => 200,
-                            'msg'  => '登录成功',
-                            'data'  => [
-                                'token' => 'abcdefghijklmnabcdefghijklmn',
-                                'user_info' => [
-                                    'id' => 999,  // 用户的UID
-                                    'username' => 'test',  // 用户名可以登录系统,不允许重复
-                                    'nickname' => '测试账号',  // 用户昵称不可以登录系统，允许重复
-                                    'avatar' => '头像地址',  // 用户的头像图片地址
-                                ]
+                    'success' => [
+                        'code' => 200,
+                        'msg'  => '登录成功',
+                        'data'  => [
+                            'token' => 'abcdefghijklmnabcdefghijklmn',
+                            'user_info' => [
+                                'id' => 999,  // 用户的UID
+                                'username' => 'test',  // 用户名可以登录系统,不允许重复
+                                'nickname' => '测试账号',  // 用户昵称不可以登录系统，允许重复
+                                'avatar' => '头像地址',  // 用户的头像图片地址
                             ]
-                        ],
-                        'error' => [
-                            'code' => 0,
-                            'msg'  => '密码错误'
                         ]
+                    ],
+                    'error' => [
+                        'code' => 0,
+                        'msg'  => '密码错误'
                     ]
                 ]
             ]
