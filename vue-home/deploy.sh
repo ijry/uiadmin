ba70603912b8ef72e37b930d1112773f378a3c03
jry <ijry@qq.com>
2019-03-10 21:14:05 +0800

    优化deploy脚本前后台一起部署

--- vue-home/deploy.sh
+++ vue-home/deploy.sh
@@ -6,17 +6,24 @@ set -e
 # 构建
 npm run build
 
+# vue-admin
+cd ../vue-admin/
+npm run build
+
+cd -
+cp -r ../vue-admin/dist ./dist/admin
+
 # cd 到构建输出的目录下
 cd dist
 
 # 部署到自定义域域名
-# echo 'www.example.com' > CNAME
+# echo 'initadmin.net' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-#git push -f git@github.com:ijry/initadmin-home.git master:gh-pages
+# git push -f git@github.com:ijry/initadmin.git master:gh-pages
 
 cd -
