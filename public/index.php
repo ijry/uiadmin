741f416db29a23f69d9edc908facec8f43780133
jry <ijry@qq.com>
2019-05-06 15:20:20 +0800

    默认utf8

--- public/index.php
+++ public/index.php
@@ -14,7 +14,8 @@
 // [ 应用入口文件 ]
 namespace think;
 
-//准许跨域请求
+// 请求头
+header("Content-type: text/html; charset=utf-8");
 header("Access-Control-Allow-Origin: * ");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type,Authorization,CloudId,CloudPid");
