1e7bc3a11c031d54d06f8364b32d2803fea39b1f
jry <ijry@qq.com>
2019-05-08 21:05:38 +0800

    路由支持前后端不分离与分离两种模式

--- application/core/behavior/Router.php
+++ application/core/behavior/Router.php
@@ -27,33 +27,47 @@ class Router
     // 行为逻辑
     public function run($params)
     {
-        //内置路由
-        Route::rule('/', 'core/index/api'); // 首页访问路由
+        // 内置路由
+        Route::rule('/', 'cms/post/index'); // 首页访问路由
         Route::rule('/api/', 'core/index/api'); // API访问路由
 
-        //计算后台API路由
+        // 计算后台API路由
         $data_list = Db::name('core_menu')
             ->removeOption('where')
             ->where('menu_type', 'in' , '1,2,3')
             ->select();
         foreach ($data_list as $key => $val) {
             $path = explode('/', $val['path']);
+            // 前后端分离路由
             Route::rule(
                 'api/' . $val['api_prefix'] . '/admin' . $val['path'] . $val['api_suffix'],
                 $path[1] . '/admin.' . $path[2] . '/' . $path[3],
                 $val['api_method']
             );
+            // 前后端不分离路由
+            Route::rule(
+                '/admin' . $val['path'] . $val['api_suffix'],
+                $path[1] . '/admin.' . $path[2] . '/' . $path[3],
+                $val['api_method']
+            );
         }
 
-        //计算前台API路由
+        // 计算前台API路由
         $data_list = Db::name('core_menu')
             ->removeOption('where')
             ->where('menu_type', '=' , '5')
             ->select();
         foreach ($data_list as $key => $val) {
             $path = explode('/', $val['path']);
+            // 前后端分离路由
             Route::rule(
                 'api/' . $val['api_prefix'] . $val['path'] . $val['api_suffix'],
+                $path[1] . '/' . $path[2] . '/' . $path[3],
+                $val['api_method']
+            );
+            // 前后端不分离路由
+            Route::rule(
+                $val['path'] . $val['api_suffix'],
                 $path[1] . '/' . $path[2] . '/' . $path[3],
                 $val['api_method']
             );
