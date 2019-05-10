e8eb9c6d57002f3a16b4b485826e7a7228840a9f
jry <ijry@qq.com>
2019-05-10 16:16:51 +0800

    session默认驱动

--- config/session.php
+++ config/session.php
@@ -20,9 +20,9 @@ return [
     // SESSION_ID的提交变量,解决flash上传跨域
     'var_session_id' => '',
     // SESSION 前缀
-    'prefix'         => 'tpvue',
+    'prefix'         => 'ia',
     // 驱动方式 支持redis memcache memcached
-    'type'           => 'redis',
+    'type'           => '',
     // redis主机
     'host'       => '127.0.0.1',
      // redis端口
