caaf01560cf95dba227b4bb3bbcce9ec71ff008f
jry <598821125@qq.com>
2019-06-01 07:10:24 +0800

    iBuilder表单支持左右分栏

--- application/core/util/ibuilder/IbuilderForm.php
+++ application/core/util/ibuilder/IbuilderForm.php
@@ -92,8 +92,11 @@ class IbuilderForm {
         $item['title'] = $title;
         $item['type'] = $type;
         $item['value'] = $value;
+        $item['temp'] = ''; // 临时使用
+        $item['show_modal'] = false; // 显示弹窗
         $extra['placeholder'] = isset($extra['placeholder']) ? $extra['placeholder'] : '';
         $extra['tip'] = isset($extra['tip']) ? $extra['tip'] : '';
+        $extra['position'] = isset($extra['position']) ? $extra['position'] : 'left';
         // 上传
         if (in_array($item['type'], ['image', 'images', 'file', 'files'])) {
             // 上传接口
