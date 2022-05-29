8013b425dde5421e9c8200f02eb5e3d24942e7ee
jry <ijry@qq.com>
2022-05-29 19:48:04 +0800

    update doc link

--- uiadmin-vue/src/pages/base.vue
+++ uiadmin-vue/src/pages/base.vue
@@ -15,7 +15,7 @@
                 </p>
                 </div>
                 <div class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-                <a target="_blank" href="https://uiadmin.jiangruyi.com/docs/uiadmin" class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"> 了解更多 </a>
+                <a target="_blank" href="https://uiadmin.jiangruyi.com/docs/uiadmin?id=227" class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"> 了解更多 </a>
                 </div>
                 <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
                 <button type="button" class="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
