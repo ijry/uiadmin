6d96b3d2dca3b7f71cfe4b5613513e63aef91300
jry <ijry@qq.com>
2022-05-31 21:09:28 +0800

    fix deploy.sh

--- uiadmin-3rd/think-swagger-ui-vuele/deploy.sh
+++ uiadmin-3rd/think-swagger-ui-vuele/deploy.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env sh
 
+set -e
+
+npm run build
+
+dir=${PWD##*/}
+
+echo $dir
+
+cd ..
+
+if [ -d "../../uiadmin-starter/public/apidoc/*" ]; then
+    rm -r ../../uiadmin-starter/public/apidoc/*
+fi
+
 cp -r ./dist/* ../../uiadmin-starter/public/apidoc/
 
-# set -e
-
-# npm run build
-
-# dir=${PWD##*/}
-
-# echo $dir
-
-# cd ..
-
-# if [ -d "../../uiadmin-starter/public/apidoc/*" ]; then
-#     rm -r ../../uiadmin-starter/public/apidoc/*
-# fi
-
-# cp -r ./dist/* ../../uiadmin-starter/public/apidoc/
-
-# echo 'success'
+echo 'success'
 
