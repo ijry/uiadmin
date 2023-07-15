9be6edbc48443e5d826aa832d12c9dee4cd2f788
jry <ijry@qq.com>
2023-07-15 09:23:58 +0800

    移除think-swagger-ui源码

--- uiadmin-3rd/think-swagger-ui-vuele/src/store/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-  swaggerPath: state => state.swagger.path,
-  swaggerInfo: state => state.swagger.info,
-  menus: state => state.swagger.menus,
-  headers: state => state.swagger.headers,
-  theme: state => state.main.theme
-
-}
-export default getters
