d77ef24eba26a79c4b2336f93d08ee1afe380bde
jry <ijry@qq.com>
2023-07-19 23:16:30 +0800

    修复启动报错

--- back-end-java/uiadmin-spring-boot/summer-boot/src/main/java/com/jiangruyi/summer/boot/SummerBootApplication.java
+++ back-end-java/uiadmin-spring-boot/summer-boot/src/main/java/com/jiangruyi/summer/boot/SummerBootApplication.java
@@ -1,11 +1,16 @@
 package com.jiangruyi.summer.boot;
 
+import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication(
+	// exclude= {DataSourceAutoConfiguration.class}
+)
+@MapperScan(basePackages = "com.**.mapper")
 public class SummerBootApplication {
 
 	public static void main(String[] args) {
