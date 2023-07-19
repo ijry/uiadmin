16519c87e415423382106016cf4b6088f62c06d8
jry <598821125@qq.com>
2023-07-19 20:47:54 +0800

    增加自动配置类

---
+++ back-end-java/uiadmin-spring-boot/summer-core/src/main/java/com/jiangruyi/summer/core/config/AutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.jiangruyi.summer.core.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+// @Configuration
+// @EnableConfigurationProperties(SummerProperties.class)
+// @ComponentScan(basePackages = "com.jiangruyi.summer.core,com.jiangruyi.summer.generator")
+public class AutoConfiguration {
+}
