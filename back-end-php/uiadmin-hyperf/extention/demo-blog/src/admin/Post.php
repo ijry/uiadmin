3870107f7df5f3f8d0c4ba2e4a33e1b390f181a6
jry <ijry@qq.com>
2023-10-30 16:59:05 +0800

    hyperf版本增加demo扩展

---
+++ back-end-php/uiadmin-hyperf/extention/demo-blog/src/admin/Post.php
@@ -0,0 +1,249 @@
+<?php
+namespace demo\blog\admin;
+
+use Hyperf\HttpServer\Contract\RequestInterface;
+use uiadmin\core\admin\BaseAdmin;
+use uiadmin\core\attributes\MenuItem;
+use demo\blog\model\Post as PostModel;
+
+/**
+ * 文章管理
+ *
+ * @author jry <ijry@qq.com>
+ */
+class Post extends BaseAdmin
+{
+    /**
+     * 文章列表
+     *
+     * @return \think\Response
+     * @author jry <ijry@qq.com>
+     */
+    #[MenuItem(["title" => "文章列表", "path" => "/blog/post/lists", "pmenu" => "/_tab_content",
+    "menuType" => 1, "menuLayer" => "admin", "routeType" => "list",
+    "apiSuffix" => "", "apiParams" => "", "apiMethod" => "GET", "sortnum" => 0])]
+    public function lists()
+    {
+        $page = input('get.page/d') ?: 1;
+        $limit = input('get.limit/d') ?: 10;
+        $where = [];
+        $keyword = input('get.keyword', '');
+
+        // 文章列表
+        $dataList = PostModel::where($where)
+            ->orderByDesc('id')
+            ->get();
+        $total = PostModel::where($where)
+            ->count();
+
+        // 构造动态页面数据
+        $xyBuilderList = new \uiadmin\core\util\xybuilder\XyBuilderList();
+        $xyBuilderList->init()
+            //->setDataPage($total, $limit, $page)
+            ->addTopButton('add', '添加文章', ['api' => '/v1/admin/blog/post/add']);
+        $listData = $xyBuilderList
+            ->addRightButton('edit', '修改', ['api' => '/v1/admin/blog/post/edit', 'title' => '修改文章信息'])
+            ->addRightButton('delete', '删除', [
+                'api' => '/v1/admin/blog/post/delete',
+                'title' => '确认要删除该文章吗？',
+                'modalType' => 'confirm',
+                'width' => '600',
+                'okText' => '确认删除',
+                'cancelText' => '取消操作',
+                'content' => '<p>删除后将清空绑定的所有登录验证记录</p>',
+            ])
+            ->addFilterItem('keyword', '关键字', 'text', $keyword, [
+            ])
+            ->addColumn('id' , 'ID', ['width' => '80px'])
+            ->addColumn('title', '标题', ['width' => '120px'])
+            ->addColumn('createTime', '创建时间', [
+                'width' => '170px',
+                'type' => 'template',
+                'template' => 'time',
+                'extend' => ['format' => 'yyyy-MM-dd hh:mm:ss']
+            ])
+            ->addColumn('status' , '状态', [
+                'width' => '80px',
+                'type' => 'template',
+                'template' => 'switch',
+                'options' => [ 1 => '正常', 0 => '禁用']
+            ])
+            ->addColumn('rightButtonList', '操作', [
+                'minWidth' => '260px',
+                'type' => 'template',
+                'template' => 'rightButtonList'
+            ])
+            ->setDataList($dataList)
+            ->getData();
+
+        // 返回数据
+        return json([
+            'code' => 200, 'msg' => '成功', 'data' => [
+                'listData' => $listData
+            ]
+        ]);
+    }
+
+    /**
+     * 添加
+     *
+     * @return \think\Response
+     * @author jry <ijry@qq.com>
+     */
+    #[MenuItem(["title" => "添加文章", "path" => "/blog/post/add", "pmenu" => "/blog/post/lists",
+        "menuType" => 2, "menuLayer" => "admin", "routeType" => "form",
+        "apiSuffix" => "", "apiParams" => "", "apiMethod" => "GET|POST", "sortnum" => 0])]
+    public function add()
+    {
+        if (Request::isMethod('post')) {
+            // 数据验证
+            $this->validateMake([
+                'title'  => 'require',
+            ],
+            [
+                'title.require' => '标题必须',
+            ]);
+            $data = Request::input();
+            $this->validate($data);
+
+            // 数据构造
+            $dataDb = $data;
+            if (count($dataDb) <= 0 ) {
+                return json(['code' => 0, 'msg' => '无数据提交', 'data' => []]);
+            }
+            $dataDb['status']   = 1;
+            $dataDb['createTime'] = date('Y-m-d');
+
+            // 存储数据
+            $ret = PostModel::create($dataDb);
+            if ($ret) {
+                return json(['code' => 200, 'msg' => '添加文章成功', 'data' => []]);
+            } else {
+                return json(['code' => 0, 'msg' => '添加文章失败', 'data' => []]);
+            }
+        } else {
+            // 构造动态页面数据
+            $xyBuilderForm = new \uiadmin\core\util\xybuilder\XyBuilderForm();
+            $formData = $xyBuilderForm->init()
+                ->setFormMethod('post')
+                ->addFormItem('title', '标题', 'text', '', [
+                    'placeholder' => '标题',
+                    'tip' => '标题'
+                ])
+                ->addFormItem('content', '内容', 'html', '', [
+                    'placeholder' => '内容',
+                    'tip' => '内容'
+                ])
+                ->addFormRule('title', [
+                    ['required' => true, 'message' => '请填写标题', 'trigger' => 'change'],
+                ])
+                ->setFormValues()
+                ->getData();
+
+            // 返回数据
+            return json([
+                'code' => 200,
+                'msg' => '成功',
+                'data' => [
+                    'formData' => $formData
+                ]
+            ]);
+        }
+    }
+
+    /**
+     * 修改
+     *
+     * @return \think\Response
+     * @author jry <ijry@qq.com>
+     */
+    #[MenuItem(["title" => "修改文章", "path" => "/blog/post/edit", "pmenu" => "/blog/post/lists",
+        "menuType" => 2, "menuLayer" => "admin", "routeType" => "form",
+        "apiSuffix" => "/{id}", "apiParams" => "", "apiMethod" => "GET|PUT", "sortnum" => 0])]
+    public function edit($id)
+    {
+        // 文章信息
+        $info = PostModel::where('id', $id)
+            ->first();
+        if (request()->isPut()) {
+            // 数据验证
+            $this->validateMake([
+                    'title'  => 'require',
+                ],
+                [
+                    'title.require' => '标题必须',
+                ]);
+            $data = Request::input();
+            $this->validate($data);
+
+            // 数据构造
+            $dataDb = $data;
+            if (count($dataDb) <= 0 ) {
+                return json(['code' => 0, 'msg' => '无数据提交', 'data' => []]);
+            }
+
+            /// 更新数据
+            foreach ($dataDb as $key => $value) {
+                if (isset($info[$key])) {
+                    $info[$key] = $value;
+                }
+            }
+
+            // 存储数据
+            $ret = $info->save();
+            if ($ret) {
+                return json(['code' => 200, 'msg' => '修改文章信息成功', 'data' => []]);
+            } else {
+                return json(['code' => 0, 'msg' => '修改文章信息失败', 'data' => []]);
+            }
+        } else {
+            // 构造动态页面数据
+            $xyBuilderForm = new \uiadmin\core\util\xybuilder\XyBuilderForm();
+            $formData = $xyBuilderForm->init()
+                ->setFormMethod('put')
+                ->addFormItem('title', '标题', 'text', '', [
+                    'placeholder' => '标题',
+                    'tip' => '标题'
+                ])
+                ->addFormItem('content', '内容', 'html', '', [
+                    'placeholder' => '内容',
+                    'tip' => '内容'
+                ])
+                ->addFormRule('title', [
+                    ['required' => true, 'message' => '请填写标题', 'trigger' => 'change'],
+                ])
+                ->setFormValues($info)
+                ->getData();
+
+            // 返回数据
+            return json([
+                'code' => 200,
+                'msg' => '成功',
+                'data' => [
+                    'formData' => $formData
+                ]
+            ]);
+        }
+    }
+
+    /**
+     * 删除
+     *
+     * @return \think\Response
+     * @author jry <ijry@qq.com>
+     */
+    #[MenuItem(["title" => "删除文章", "path" => "/blog/post/delete", "pmenu" => "/blog/post/lists",
+        "menuType" => 3, "menuLayer" => "admin", "routeType" => "form",
+        "apiSuffix" => "/{id}", "apiParams" => "", "apiMethod" => "DELETE", "sortnum" => 0])]
+    public function delete($id)
+    {
+        // 删除文章
+        $ret = PostModel::where('id', $id)
+            ->delete();
+        if ($ret) {
+            return json(['code' => 200, 'msg' => '删除成功', 'data' => []]);
+        } else {
+            return json(['code' => 0, 'msg' => '删除错误', 'data' => []]);
+        }
+    }
+}
---
+++ back-end-php/uiadmin-hyperf/extention/demo-blog/src/database/migrations/2021_04_11_000000_create_xy_demo_blog_post_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Hyperf\Database\Schema\Schema;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Migrations\Migration;
+
+class CreateXyDemoBlogPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('xy_demo_blog_post', function (Blueprint $table) {
+            $table->integer('id', true)->unique('id')->comment('ID');
+            $table->string('namespace', 32)->default('uiadmin')->comment('根命名空间');
+            $table->string('module', 32)->default('')->comment('所属模块');
+            $table->string('title', 32)->default('')->comment('名称');
+            $table->string('icon', 32)->default('')->comment('图标');
+            $table->string('path', 32)->default('')->comment('路由路径');
+            $table->string('pmenu', 32)->default('')->comment('父菜单');
+            $table->integer('sortnum')->default(0)->comment('排序');
+            $table->string('menu_layer', 32)->default('')->comment('分层标记');
+            $table->integer('menu_type')->default(0)->comment('菜单类型1导航2按钮3仅接口');
+            $table->string('route_type', 32)->default('')->comment('路由类型');
+            $table->string('api_prefix', 15)->default('')->comment('接口前缀, 一般为：v1');
+            $table->string('api_suffix', 32)->default('')->comment('接口路由参数后缀');
+            $table->string('api_params', 32)->default('')->comment('接口Query参数');
+            $table->string('out_url')->default('')->comment('外链地址或者远程组件地址');
+            $table->boolean('is_dev')->default(false)->comment('是否开发者模式才显示');
+            $table->dateTime('create_time')->nullable()->comment('创建时间');
+            $table->dateTime('update_time')->nullable()->comment('更新时间');
+            $table->boolean('status')->default(false)->comment('删除状态，0已禁用1正常');
+            $table->string('tip')->default('')->comment('Tip');
+            $table->string('api_method', 32)->default('')->comment('接口请求方法');
+            $table->boolean('is_hide')->default(false)->comment('是否隐藏菜单');
+            $table->text('doc')->nullable()->comment('接口文档');
+            $table->string('api_ext', 32)->default('')->comment('接口虚拟后缀');
+            $table->boolean('delete_time')->default(false)->comment('删除时间');
+        });
+	}
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::dropIfExists('xxx');
+    }
+}
