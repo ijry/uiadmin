74decc94bac7f4b8c60b9858c5b9a6a5b63abdb5
jry <ijry@qq.com>
2024-10-29 22:30:58 +0800

    修复前端文件夹名存在空格导致无法启动uni-app

--- front-end/uiadmin-uniapp/main.js
+++ front-end/uiadmin-uniapp/main.js
@@ -1,16 +1,6 @@
 import App from './App'
 import uviewPlus from 'uview-plus'
 
-// #ifndef VUE3
-import Vue from 'vue'
-Vue.config.productionTip = false
-App.mpType = 'app'
-const app = new Vue({
-    ...App
-})
-app.$mount()
-// #endif
-
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
 export function createApp() {
