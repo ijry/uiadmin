8a013c716ba56ead3edeaedabd188ee241dc29cd
jry <ijry@qq.com>
2025-07-06 11:46:04 +0800

    增加pinia

--- front-end/uiadmin-uniapp/main.js
+++ front-end/uiadmin-uniapp/main.js
@@ -1,25 +1,18 @@
 import App from './App'
 import 'uno.css';
 
+// pinia
+import pinia from '@/store'
+
 // 引入全局uview-plus
-import uviewPlus, { setConfig } from '@/uni_modules/uview-plus'
+import uviewPlus from '@/uni_modules/uview-plus'
 
-// #ifndef VUE3
-import Vue from 'vue'
-import './uni.promisify.adaptor'
-Vue.config.productionTip = false
-App.mpType = 'app'
-const app = new Vue({
-  ...App
-})
-
-app.$mount()
-// #endif
 
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
+  app.use(pinia)
   app.use(uviewPlus)
   return {
     app
